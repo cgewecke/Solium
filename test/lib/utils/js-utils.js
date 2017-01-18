4c5ab19a4a0376f857d7ba8af152992596062c51
cgewecke <christophergewecke@gmail.com>
2017-01-18 13:24:13 -0800

    Add travis.yml. Add wrapper methods & their unit tests to utils/js-utils

--- test/lib/utils/js-utils.js
+++ test/lib/utils/js-utils.js
@@ -5,7 +5,13 @@
 
 'use strict';
 
-var jsUtils = require ('../../../lib/utils/js-utils');
+var jsUtils = require ('../../../lib/utils/js-utils'),
+	Solium = require ('../../../lib/solium');
+
+var userConfig = {
+	'custom-rules-filename': null,
+	rules: {}
+};
 
 describe ('Test jsUtils functions', function () {
 
@@ -13,10 +19,15 @@ describe ('Test jsUtils functions', function () {
 		jsUtils.should.have.ownProperty ('isStrictlyObject');
 		jsUtils.isStrictlyObject.should.be.type ('function');
 
+		jsUtils.should.have.ownProperty ('toContract');
+		jsUtils.toContract.should.be.type ('function');
+
+		jsUtils.should.have.ownProperty ('toFunction');
+		jsUtils.toFunction.should.be.type ('function');
 		done ();
 	});
 
-	it ('should correctly classify whether argument is a non-array, non-null object', function (done) {
+	it ('isStrictlyObject: should correctly classify whether argument is a non-array, non-null object', function (done) {
 		var iso = jsUtils.isStrictlyObject;
 
 		iso ().should.equal (false);
@@ -30,4 +41,41 @@ describe ('Test jsUtils functions', function () {
 		done ();
 	});
 
+	it('toContract: should correctly wrap a solidity statement in contract code', function (done) {
+		var toContract = jsUtils.toContract;
+		var statement = 'uint x = 1;'
+		var expected = 
+			'pragma solidity ^0.4.3;\n\n' +
+			'contract Wrap {\n' +
+			'\t' + statement + '\n' +
+			'}';
+
+		var errors = Solium.lint(expected, userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		toContract(statement).should.equal(expected);
+
+		Solium.reset();
+		done ();
+	});
+
+	it('toFunction: should correctly wrap a solidity statement in contract/function code', function (done) {
+		var toFunction = jsUtils.toFunction;
+		var statement = 'uint x = 1;'
+		var expected = 
+			'pragma solidity ^0.4.3;\n\n' +
+			'contract Wrap {\n' +
+			'\tfunction wrap() {\n' + 
+			'\t\t' + statement + '\n' +
+			'\t}\n' +
+			'}';
+
+		var errors = Solium.lint(expected, userConfig);
+		errors.constructor.name.should.equal ('Array');
+		errors.length.should.equal (0);
+		toFunction(statement).should.equal(expected);
+
+		Solium.reset();
+		done ();
+	});
 });
